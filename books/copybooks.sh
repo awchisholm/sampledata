172be30926ef12c9486e4dcdb38937729d1d5f5e
Andrew Chisholm <awchisholm@users.noreply.github.com>
2024-03-05 12:15:26 +0000

    Update copybooks.sh

--- books/copybooks.sh
+++ books/copybooks.sh
@@ -4,4 +4,4 @@ wget -q -O Baum_LostPrincess.txt https://raw.githubusercontent.com/awchisholm/sa
 wget -q -O Baum_WonderfulWizardOfOz.txt https://raw.githubusercontent.com/awchisholm/sampledata/master/books/Baum_WonderfulWizardOfOz.txt
 wget -q -O ConanDoyle_AdventureOfTheDevilsFoot.txt https://raw.githubusercontent.com/awchisholm/sampledata/master/books/ConanDoyle_AdventureOfTheDevilsFoot.txt
 wget -q -O ConanDoyle_DisappearanceOfLadyFrancesCarfax.txt https://raw.githubusercontent.com/awchisholm/sampledata/master/books/ConanDoyle_DisappearanceOfLadyFrancesCarfax.txt
-wget -q -O ConanDoyle_SignOfTheFour.txt ttps://raw.githubusercontent.com/awchisholm/sampledata/master/books/ConanDoyle_SignOfTheFour.txt
+wget -q -O ConanDoyle_SignOfTheFour.txt https://raw.githubusercontent.com/awchisholm/sampledata/master/books/ConanDoyle_SignOfTheFour.txt
